6c960fea2fb7036d8b398281f6a0184ed6bf70aa
Dawood-Abbas <dawoodabbas91@gmail.com>
2022-06-14 16:18:10 -0700

    Initial commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,6 +1,13 @@
-#include <main.h>
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Description: Prints _putchar
+ *
+ * Return: return 0 always success
+*/
 
-main()
+int main(void)
 {
 	printf("_putchar\n");
 	return 0;
