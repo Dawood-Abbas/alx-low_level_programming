f4982ba28e280ef27096584313e1835b7ccd6160
Dawood-Abbas <dawoodabbas91@gmail.com>
2022-06-14 16:22:31 -0700

    Initial commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,10 +5,17 @@
  * Description: Prints _putchar
  *
  * Return: return 0 always success
-*/
-
+ */
 int main(void)
 {
-	printf("_putchar\n");
-	return 0;
+    _putchar('_');
+    _putchar('p');
+    _putchar('u');
+    _putchar('t');
+    _putchar('c');
+    _putchar('h');
+    _putchar('a');
+    _putchar('r');
+    _putchar('\n');
+    return (0);
 }
