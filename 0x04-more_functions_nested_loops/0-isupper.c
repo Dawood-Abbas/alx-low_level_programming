85972c1206ea897b92e93da13d4bd2a7247468df
Dawood-Abbas <dawoodabbas91@gmail.com>
2022-06-21 23:04:38 -0700

    First commit

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,7 +1,6 @@
 #include "main.h"
 
-/** 
- * * _isupper - checks for the uppercase character
+/** _isupper - checks for the uppercase character
  * * @c: character that is checked
  * * Return: 1 if uppercase return 0 if otherwise
  */
